b2f345d832d37b75b217a90259c87e2e1cab7460
Andrew Parnham <atparnham@gmail.com>
2019-12-17 10:42:44 +0000

    Working with annotations

---
+++ annotations/annotations.ts
@@ -0,0 +1,14 @@
+//Primitives
+const myString: string = "Some string";
+const myNumber: number = 7;
+const myBoolean: boolean = true;
+const myNull: null = null;
+const myUndefined: undefined = undefined;
+
+//Dates
+const now: Date = new Date();
+
+//Arrays
+const arrayOfStrings: string[] = ["hello", "world"];
+const arrayOfNumbers: number[] = [1, 2, 3];
+const arrayOfBooleans: boolean[] = [true, false];
