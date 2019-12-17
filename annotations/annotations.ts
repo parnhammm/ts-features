910f30f8fe4c874708fde5adab2817d305e8e95a
Andrew Parnham <atparnham@gmail.com>
2019-12-17 11:11:32 +0000

    Adding in annotation example for functions and adding new file on when to use annotations.

--- annotations/annotations.ts
+++ annotations/annotations.ts
@@ -5,10 +5,36 @@ const myBoolean: boolean = true;
 const myNull: null = null;
 const myUndefined: undefined = undefined;
 
-//Dates
+//Built in objects
 const now: Date = new Date();
 
 //Arrays
 const arrayOfStrings: string[] = ["hello", "world"];
 const arrayOfNumbers: number[] = [1, 2, 3];
 const arrayOfBooleans: boolean[] = [true, false];
+
+//Classes
+class Car {}
+const myCar: Car = new Car();
+
+//Object literals
+//Note that here we have to fully define out what our object is for typescript
+let point: { x: number; y: number; z: number } = {
+  x: 0,
+  y: 0,
+  z: 0
+};
+
+//Functions
+
+//Without any annotations the function declaration would look like this...
+const logNumberNoAnnotation = (i: number) => {
+  console.log(i);
+};
+
+//We want to annotate what arguments the function takes and the
+//return types we expect. We give this function a number, and return nothing
+//so here we will use a special type of 'void'
+const logNumberWithAnnotation: (i: number) => void = (i: number) => {
+  console.log(i);
+};
