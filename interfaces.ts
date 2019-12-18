3ee95d631d312232f2524991cc4fb5f289709541
Andrew Parnham <atparnham@gmail.com>
2019-12-18 10:10:33 +0000

    Adding in interfaces file

---
+++ interfaces.ts
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////////////////
+// Interfaces
+//
+// Documents interfaces in typescripe
+////////////////////////////////////////////////////////////////
+
+//Start with an plain object
+const oldCivic = {
+  name: "Civic",
+  year: 2000,
+  broken: true
+};
+
+//Now lets create a function to print details about a vehicle out
+//To avoid the 'any' type, we will need to create a object literal
+const printVehicleDetails = (vehicle: {
+  name: string;
+  year: number;
+  broken: boolean;
+}): void => {
+  console.log(`
+      Name: ${vehicle.name}
+      Year: ${vehicle.year}
+      Broken: ${vehicle.broken}
+  `);
+};
+
+printVehicleDetails(oldCivic);
+
+//However the problem here is threefold:
+// (1) the type annotation is really long and hard to read
+// (2) if we add more properties we will have to do a lot more refactoring
+// (3) we would need to repeat this long annotation everytime we want to use it
+
+//Rewrite this using an interface
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+const newCivic = {
+  name: "Civic",
+  year: 2019,
+  broken: false
+};
+
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(`
+      Name: ${vehicle.name}
+      Year: ${vehicle.year}
+      Broken: ${vehicle.broken}
+  `);
+};
