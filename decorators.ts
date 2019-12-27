d53cac4ba5bee1988e8ad4b6122b060cc433b9b2
Andrew Parnham <atparnham@gmail.com>
2019-12-27 21:56:07 +0000

    Decorator docs

---
+++ decorators.ts
@@ -0,0 +1,61 @@
+////////////////////////////////////////////////////////////////
+// Decorators
+//
+// Documents decorators in typescript (still experimental)
+//
+// Decorators on a property, method, accessor:
+//   * First argument is the prototype of the object
+//   * Second argumant is the key of the property/method/accessor on the object
+//   * Third argumnet is the property descriptor
+//   * Decorators are applied when the code is ran, not when an instance is created
+////////////////////////////////////////////////////////////////
+
+class Boat {
+  //Property
+  colour: string = "white";
+
+  //Accessor
+  get formattedColour(): string {
+    return `This boats colour is ${this.colour}`;
+  }
+
+  //Method
+  @logError
+  pilot(): void {
+    throw new Error("blah");
+  }
+
+  @logErrorFactory("Error thrown in pilotWithDecorator")
+  pilotWithDecorator(): void {
+    throw new Error("bleh");
+  }
+}
+
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  //Now a reference to pilot()
+  const method = desc.value;
+
+  desc.value = function() {
+    try {
+      method();
+    } catch (e) {
+      console.log("Error caught: " + e);
+    }
+  };
+}
+
+//Decorator factory allows you to customise the decorator further
+function logErrorFactory(errorMessage: string) {
+  return function(target: any, key: string, desc: PropertyDescriptor): void {
+    //Now a reference to pilot()
+    const method = desc.value;
+
+    desc.value = function() {
+      try {
+        method();
+      } catch (e) {
+        console.log("Error caught: " + errorMessage);
+      }
+    };
+  };
+}
