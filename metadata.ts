fd141bd94f1ada732a9222153dc1ed38e0fcb493
Andrew Parnham <atparnham@gmail.com>
2019-12-28 13:59:23 +0000

    Further examples of metadata

--- metadata.ts
+++ metadata.ts
@@ -21,5 +21,36 @@ const metaDataRetrieved = Reflect.getMetadata("note", plane);
 console.log(metaDataRetrieved);
 
 //We can associate metadata to a property as well
-Reflect.defineMetadata('note', 'more notes', plane, 'colour');
-const propNote = Reflect.getMetadata('note', plane, 'colour')
+Reflect.defineMetadata("note", "more notes", plane, "colour");
+const propNote = Reflect.getMetadata("note", plane, "colour");
+
+//Classes with decorators...
+@printMetaData
+class Plane {
+  colour: string = "white";
+
+  @markFunction("abc123")
+  fly(): void {
+    console.log("Flying");
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function(target: Plane, key: string) {
+    Reflect.defineMetadata("secret", secretInfo, target, key);
+  };
+}
+
+//Get the data using prototype
+const secret = Reflect.getMetadata("secret", Plane.prototype, "fly");
+console.log(`Secret is ${secret}`);
+
+//Class annotations refer to the constructor of the object...
+function printMetaData(target: typeof Plane) {
+  for (let key in target.prototype) {
+    //Iterate through all the keys on the prototype and
+    //see if we can find the secret metadata
+    const secret = Reflect.getMetadata("secret", target.prototype, key);
+    console.log(`Secret is ${secret}`);
+  }
+}
